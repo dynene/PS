09284414275557fdd007816860d81df3f31a18e6
Dyne Kim <69078708+dynene@users.noreply.github.com>
2024-04-16 23:25:05 +0900

    [Gold IV] Title: 타일 채우기, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -1,47 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /*
- * 231018
- * BOJ 2133 타일 채우기
- * 골드 4
- * 
- * 메모리 KB
- * 시간 ms
+240416
+BJ2133 타일 채우기 - 골드 4
  */
-
-import java.io.*;
-import java.util.*;
-
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		/*
-		 * 홀수 : 절대 완성할 수 없음 (무조건 0)
-		 */
-		
-		if(n%2==1) {
-			System.out.println(0);
-			return;
-		}
-		else if (n==2) {
-			System.out.println(3);
-			return;
-		}
-		// n이 짝수인 경우
-		int[] dp = new int[n+1];
-		dp[2] = 3;
-		dp[4] = 11;
-		
-		int sum = dp[2];
-		for(int i=4;i<=n;i+=2) {
-			dp[i] = sum*2 + 2 + dp[i-2];
-			sum+=dp[i];
-		}
-		
-		
-		System.out.println(dp[n]);
-		
-	}
 
+    // 3*n 을 1*2와 2*1 타일로 채우는 경우의 수
+    // n 최대 30
+
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(in.readLine());
+
+        if (n % 2 == 1) {
+            System.out.println(0);
+            return;
+        } else if (n == 2) {
+            System.out.println(3);
+            return;
+        }
+
+        dp = new int[n + 1];
+
+        // 여기서 n은 무조건 짝수임 ..
+        dp[2] = 3;
+        dp[4] = 11;
+        int sum = dp[2];
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = sum * 2 + dp[i-2] + 2;
+            sum += dp[i];
+        }
+
+        System.out.println(dp[n]);
+    }
 }
