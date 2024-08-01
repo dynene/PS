89e1865bfbea10cf3b6c36f4dcccaea4254e2ecf
Dyne Kim <69078708+dynene@users.noreply.github.com>
2024-08-02 04:03:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 452 ms, Memory: 91512 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -2,85 +2,62 @@ import java.util.*;
 import java.io.*;
 
 /*
-240414
-BJ 9935 문자열 폭발 - 골드 4
+240801
+BJ9935 문자열 폭발 - 골4
+다시 풀기~
  */
+
 public class Main {
 
-    /*
-    폭발 문자열은 같은 문자를 두 개 이상 포함하지 않는다.
-    남아있는 문자가 없는 경우 "FRULA"를 출력
-     */
+    static String str;
+    static String boomStr;
 
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String str = in.readLine();
-        String boomstr = in.readLine();
-        int boomlen = boomstr.length();
 
-        // 폭발 문자열의 각 글자에 인덱스를 부여 (어차피 중복 글자 없으니까)
-        // 매 글자마다, 인덱스 판단해서 이어지는 경우 스택2에 쌓기
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < boomlen; ++i) {
-            map.put(boomstr.charAt(i), i); // 0부터... boomstr 길이까지... 넣기...
-        }
+        str = in.readLine();
+        boomStr = in.readLine();
 
         Stack<Character> stack = new Stack<>();
-        Stack<Integer> boomstack = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
 
-        // 문제: 폭발 문자열 길이가 1일 때를 고려해야 함 !!!
-        // 어떻게?...
-        //
-
+        int curCnt = 0;
         for (int i = 0; i < str.length(); ++i) {
-            char cur = str.charAt(i);
+            char c = str.charAt(i);
+            stack.push(c);
 
-            // 어쨋든 일단 넣음
-            stack.push(cur);
+            if (c == boomStr.charAt(curCnt)) {
+                curCnt++;
 
-            // 폭발 문자열에 포함되는 글자인 경우
-            if (map.containsKey(cur)) {
-                // 1번째 글자이거나 (value가 0이거나)
-                // boomstack의 top + 1 글자이거나 (value가 boomstack.peek() + 1 이거나)
-                int value = map.get(cur);
-
-                if (value == 0 || (!boomstack.isEmpty() && value == boomstack.peek() + 1)) { // 첫번째 문자인 경우 OR 순서 맞게 쌓인 경우
-                    boomstack.push(value);
-
-                    if (boomstack.peek() == boomlen - 1) { // 폭발 문자열 다 쌓인 경우
-                        // boomstack에서 빼고..
-                        // stack에서도 빼고..
-                        for (int j = 0; j < boomlen; ++j) {
-                            stack.pop();
-                            boomstack.pop();
-                        }
+                if (curCnt == boomStr.length()) { // 다 모였으면.. 터트리기..
+                    for (int j = 0; j < boomStr.length(); ++j) {
+                        stack.pop();
                     }
-                } else { // 폭발 문자열에 포함되지만, 폭발 조건에 맞지 않는 경우!
-                    boomstack.clear();
-                }
-            } else { // 포함되지 않는 글자인 경우
-                // 이때... 쌓이고 있던 boomstack이 있다면 처리를 해 줘야 한다.
-                boomstack.clear();
-            }
 
-//
-//            System.out.println(stack.toString());
-//            System.out.println(boomstack.toString());
-//            System.out.println();
+                    if (!stack2.isEmpty() && stack.peek() == boomStr.charAt(stack2.peek() - 1)) {
+                        curCnt = stack2.pop();
+                    } else {
+                        curCnt = 0;
+                    }
+                }
+            } else if (c == boomStr.charAt(0)) {
+                stack2.push(curCnt); // 지금까지 쌓던 거 넣어놓고...
+                curCnt = 1;
+            } else {
+                // stack2에 쌓인 거 다 지워야 함
+                stack2.clear();
+                curCnt = 0;
+            }
         }
 
         if (stack.isEmpty()) {
             System.out.println("FRULA");
         } else {
             StringBuilder sb = new StringBuilder();
-            while(!stack.isEmpty()) {
+            while (!stack.isEmpty()) {
                 sb.append(stack.pop());
             }
             System.out.println(sb.reverse());
         }
-
-
     }
-
 }
